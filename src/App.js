1c40502eb7fa8a33239414fda8ce1df04c93e721
DaisyyWard <daisy.w@mmtdigital.co.uk>
2022-03-27 21:25:51 +0100

    Add button, favicons, notes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import './App.css';
+import './App.css'
+import './components/button.css'
+
+import React, { useState } from 'react'
 
 import Input from './components/Input'
 import Dropdown from './components/Dropdown'
 
 function App() {
+  const [errorState, setErrorState] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Accessibility</h1>
+        {/* Accessibile logo */}
+        <h1>Daisy's Accessibility Demo</h1>
+        {/* Decorative Image */}
       </header>
       <section>
         <h2>Personal Details</h2>
-        <div class='input-wrapper'>
+        <div className='input-wrapper'>
           <Input
             id='firstName'
             label='First Name'
             required
-            hasError={false}
+            hasError={errorState}
+            errorMessage={'Please enter your first name'}
           />
           <Input
             id='lastName'
             label='Last Name'
             required
-            hasError={false}
+            hasError={errorState}
+            errorMessage={'Please enter your last name'}
           />
         </div>
         <Dropdown />
+        {/* Button for a modal popup form
+          will including button as a link
+          Roving tab
+        */}
+        {/* Another h tag, not in order */}
+        {/*
+            Custom toggle button
+          */}
+        {/*  */}
+        {/* Button to trigger loading spinner */}
+        <button className='button submit-button' aria-label='Submit personal details form' onClick={() => setErrorState(!errorState)}>Submit</button>
       </section>
     </div>
   );
